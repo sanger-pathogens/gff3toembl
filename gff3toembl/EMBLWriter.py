39f5e1d2205ed46c36c9da22df7352bf28e50b17
andrewjpage <andrewjpage@gmail.com>
2014-11-10 10:24:18 +0000

    create a chromosome.txt file

--- gff3toembl/EMBLWriter.py
+++ gff3toembl/EMBLWriter.py
@@ -1,6 +1,8 @@
 import sys
 from collections import defaultdict
 from gt import GFF3InStream
+import os
+import re
                
 from gff3toembl import convert
 from gff3toembl.EMBLConverter import EMBLConverter
@@ -8,7 +10,7 @@ from gff3toembl.VisitorStream import VisitorStream
  
 class EMBLWriter(object):
 
-    def __init__(self, gff3_file, organism, taxonid, project, description, authors, title,  publication, genome_type, classification, submitter_name, submitter_title,  submitter_location, output_filename, locus_tag = None, translation_table = 11):
+    def __init__(self, gff3_file, organism, taxonid, project, description, authors, title,  publication, genome_type, classification, submitter_name, submitter_title,  submitter_location, output_filename, locus_tag = None, translation_table = 11, chromosome_list = None):
         self.locus_tag          = locus_tag
         self.translation_table  = translation_table
         self.converter          = convert.Convert(locus_tag,translation_table)
@@ -27,6 +29,7 @@ class EMBLWriter(object):
         self.submitter_title    = submitter_title   
         self.submitter_location = submitter_location
         self.output_filename    = output_filename
+        self.chromosome_list    = chromosome_list
         
  
  
@@ -50,6 +53,29 @@ class EMBLWriter(object):
             target.write("//\n")
             i +=1
         target.close()
+        
+    def create_chromosome_list(self, chromosome_list_filename, embl_filename):
+        if chromosome_list_filename == None:
+          return
+        if not os.path.exists(embl_filename):
+          return
+        
+        embl_file = open(embl_filename, 'r')
+        chromosome_list_file = open(chromosome_list_filename, 'w')
+        object_accessions = []
+        
+        for embl_line in embl_file.readlines():
+          m = re.search('AC   * _([\w]+)', embl_line)
+          if m.group(0):
+            object_accessions.append(m.group(0))
+        
+        for index, object_accession in enumerate(object_accessions)
+          chromosome_name = str((index+1))
+          # TODO make it work for more than just Bacteria
+          chromosome_type = "Chromosome"
+          if index > 1:
+            chromosome_type = "Plasmid"
+          chromosome_list_file.write(object_accession + "\t" + chromosome_name + "\t" + chromosome_type + "\n")
 
     def parse_and_run(self):
         ins = GFF3InStream(self.gff3_file)  
@@ -61,4 +87,5 @@ class EMBLWriter(object):
             print e
             exit(1)
         self.create_output_file(self.conv.seqs.keys(), self.organism, self.taxonid, self.project, self.description, self.authors, self.title, self.publication, self.genome_type, self.classification, self.submitter_name, self.submitter_title, self.submitter_location)
+        self.create_chromosome_list(self.chromosome_list, self.output_filename)
 
