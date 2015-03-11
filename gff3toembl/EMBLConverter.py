d4773f388876a7904970f18b8c7ed33c6bad3e2f
Ben Taylor <bewt85@gmail.com>
2015-03-11 11:36:47 +0000

    Refactor out function to check if a feature is seen

--- gff3toembl/EMBLConverter.py
+++ gff3toembl/EMBLConverter.py
@@ -11,14 +11,21 @@ class EMBLConverter(CustomVisitor):
         self.feats = defaultdict(lambda: [], {})
         self.regions = []
         self.converter =  converter
-        self.features_seen = {}
+        self.features_seen = []
+
+    def update_features_seen(self, sequence_id, feature_type, feature_start, feature_end):
+      unique_feature_reference = "%s_%s_%s_%s" % (sequence_id, feature_type, feature_start, feature_end)
+      if unique_feature_reference in self.features_seen:
+        return True
+      else:
+        self.features_seen.append(unique_feature_reference)
+        return False
 
     def visit_feature_node(self, feature_node):
         feature_string = self.converter.construct_feature(feature_type = feature_node.get_type(), start = feature_node.get_start(), end = feature_node.get_end(), strand = feature_node.get_strand(), feature_attributes = feature_node.attribs)
         if feature_string != '':
-          feature_seq_coords = str(feature_node.get_seqid()) + "_"+ str(feature_node.get_start()) + "_" +str(feature_node.get_end())
-          if feature_seq_coords not     in self.features_seen :          
-            self.features_seen[feature_seq_coords] = 1
+          if not self.update_features_seen(feature_node.get_seqid(), feature_node.get_type(), 
+                                      feature_node.get_start(), feature_node.get_end()):
             self.feats[feature_node.get_seqid()].append(feature_string)
 
     def visit_region_node(self, region_node):
---
+++ gff3toembl/tests/EMBLConverter_test.py
@@ -0,0 +1,14 @@
+import unittest
+from gff3toembl.EMBLConverter import EMBLConverter
+
+class TestEMBLConverter(unittest.TestCase):
+
+    def test_update_features_seen(self):
+      converter = EMBLConverter(None)
+      assert converter.update_features_seen("SEQ_ID", "FEATURE_TYPE", 1, 100) == False
+      assert converter.update_features_seen("SEQ_ID", "FEATURE_TYPE", 1, 100) == True
+      assert converter.update_features_seen("ANOTHER_SEQ_ID", "FEATURE_TYPE", 1, 100) == False
+      assert converter.update_features_seen("SEQ_ID", "FEATURE_TYPE", 10, 110) == False
+      assert converter.update_features_seen("SEQ_ID", "ANOTHER_FEATURE_TYPE", 1, 100) == False 
+      assert converter.update_features_seen("ANOTHER_SEQ_ID", "ANOTHER_FEATURE_TYPE", 1001, 1101) == False
+      assert converter.update_features_seen("SEQ_ID", "FEATURE_TYPE", 1, 100) == True
