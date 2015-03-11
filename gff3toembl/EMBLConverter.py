041eff2da249f2ae023d608baefbdcb294c32ae5
Ben Taylor <bewt85@gmail.com>
2015-03-11 10:55:17 +0000

    Change variables names in EMBLConverter

--- gff3toembl/EMBLConverter.py
+++ gff3toembl/EMBLConverter.py
@@ -13,19 +13,19 @@ class EMBLConverter(CustomVisitor):
         self.converter =  converter
         self.features_seen = {}
 
-    def visit_feature_node(self, fn):
-        feature_string = self.converter.construct_feature(feature_type = fn.get_type(), start = fn.get_start(), end = fn.get_end(), strand = fn.get_strand(), feature_attributes = fn.attribs)
+    def visit_feature_node(self, feature_node):
+        feature_string = self.converter.construct_feature(feature_type = feature_node.get_type(), start = feature_node.get_start(), end = feature_node.get_end(), strand = feature_node.get_strand(), feature_attributes = feature_node.attribs)
         if feature_string != '':
-          feature_seq_coords = str(fn.get_seqid()) + "_"+ str(fn.get_start()) + "_" +str(fn.get_end()) 
+          feature_seq_coords = str(feature_node.get_seqid()) + "_"+ str(feature_node.get_start()) + "_" +str(feature_node.get_end())
           if feature_seq_coords not     in self.features_seen :          
             self.features_seen[feature_seq_coords] = 1
-            self.feats[fn.get_seqid()].append(feature_string)
+            self.feats[feature_node.get_seqid()].append(feature_string)
 
     def visit_region_node(self, rn):
         self.regions.append
 
-    def visit_comment_node(self, cn):
+    def visit_comment_node(self, comment_node):
         pass  # for now
 
-    def visit_sequence_node(self, sn):
-        self.seqs[sn.get_description()] = sn.get_sequence()
+    def visit_sequence_node(self, sequence_node):
+        self.seqs[sequence_node.get_description()] = sequence_node.get_sequence()
