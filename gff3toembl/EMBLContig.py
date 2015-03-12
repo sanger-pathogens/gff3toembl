602b8e1cadb88beb3cd57d19eeaa1d7cfd969b18
Ben Taylor <bewt85@gmail.com>
2015-03-12 13:29:59 +0000

    WIP reformat eC_number feature_type

--- gff3toembl/EMBLContig.py
+++ gff3toembl/EMBLContig.py
@@ -27,6 +27,12 @@ class EMBLFeature(object):
   def should_ignore_feature(self, feature_type):
     return feature_type in ['ID', 'protein_id']
 
+  def reformat_feature_type(self, feature_type):
+    if feature_type == 'eC_number':
+      return 'EC_number'
+    else:
+      return feature_type
+
 class EMBLHeader(object):
   def __init__(self,
                authors="Pathogen Genomics",
