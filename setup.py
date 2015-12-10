c95e11d2d9ec2da8456361ddf0b0b0edaf1c8a82
andrewjpage <andrewjpage@gmail.com>
2015-12-10 16:39:52 +0000

    inc version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='gff3toembl',
-    version='1.0.2',
+    version='1.0.3',
     description='Convert a GFF3 file to EMBL format for submission',
     long_description=read('README.md'),
     packages = find_packages(),
